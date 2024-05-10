265ecc3e50fc35943036ca346214959083bfb2eb
Daniel Reyes <128719578+thanyeal@users.noreply.github.com>
2024-05-10 23:52:19 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# PUPQC-FARMSv2.1
+ testing repo for debugging purposes of FARMS version 2.1
---
+++ webpack.mix.js
@@ -0,0 +1,3 @@
+mix.js('resources/js/app.js', 'resources/js')
+    .sass('resources/sass/app.scss', 'resources/css')
+    .copy('node_modules/bootstrap/dist/css/bootstrap.min.css', 'resources/css');
